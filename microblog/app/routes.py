3a835e48e52541f679f74dab6c27277821114694
Fernanda Vilela <fegvilela@gmail.com>
2020-10-13 20:54:27 -0300

    add templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Alo mundao!"
-
+    user = {'username': 'Fernanda'}
+    posts = [
+        {
+            'author': {'username': 'Luciana'},
+            'body': 'Dia maneiro aqui'
+        },
+        {
+            'author': {'username': 'Isabela'},
+            'body': 'The Avengers é maneiro!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
